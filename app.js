b057580da90cdbd3dbee66493cb8d00f36351d82
kbriannicolas <karaim.brian@gmail.com>
2023-01-02 20:58:31 -0300

    agregando views + styles

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ app.use(express.static(path.resolve(__dirname,'./public')));
 app.listen(3000,()=>console.log("listening on port 3000"));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'./views/home.html')));
+app.get('/register.html',(req,res)=>res.sendFile(path.join(__dirname,'./views/register.html')));
+app.get('/login.html',(req,res)=>res.sendFile(path.join(__dirname,'./views/login.html')));
