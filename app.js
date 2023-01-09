624b0a89a8ff0337c39ce611b38ef1e0993a91bb
kbriannicolas <karaim.brian@gmail.com>
2023-01-09 10:05:36 -0300

    cambios en app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path=require('path');
 const app=express();
 
 app.use(express.static(path.resolve(__dirname,'./public')));
-app.listen(3000,()=>console.log("listening on port 3000"));
+const port=process.env.PORT||3001;
+app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`))
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'./views/home.html')));
 app.get('/creatucuenta',(req,res)=>res.sendFile(path.join(__dirname,'./views/register.html')));
