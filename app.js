2bae14d78d6b14ad062dd49e9bd506bfc5c5fedb
kbriannicolas <karaim.brian@gmail.com>
2023-01-09 22:08:49 -0300

    modificando para deploy

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path=require('path');
 const app=express();
 
 app.use(express.static(path.resolve(__dirname,'./public')));
-const port=process.env.PORT||3001;
-app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`))
+
+const port= process.env.PORT || 3001;
+app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'./views/home.html')));
 app.get('/creatucuenta',(req,res)=>res.sendFile(path.join(__dirname,'./views/register.html')));
